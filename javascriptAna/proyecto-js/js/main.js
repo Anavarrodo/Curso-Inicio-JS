8fb34cd94fa1cd1876ef748150294fda3362adb6
PedroDLF <A.navarrodo@gmail.com>
2018-12-18 18:49:30 +0100

    PESADA

---
+++ javascriptAna/proyecto-js/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    console.log("Cargado")
+    $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 1200,
+    responsive: true,
+    
+  });
+})
