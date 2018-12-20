b3e296bd0e504b906cbaaf350b1203df1b4a2ebd
PedroDLF <A.navarrodo@gmail.com>
2018-12-20 14:49:41 +0100

    cc

---
+++ javascriptAna/PROYECTO/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    console.log("cargado")
+
+    /* SLIDER */
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1200,
+        responsive: true,
+      });
+
+    /* POSTS */
+  
+})
